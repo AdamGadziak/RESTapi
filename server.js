67f283377d33beeece5e624eba0fa3a9423d8851
Adam <adamgadziak@gmail.com>
2016-06-11 15:35:58 +0200

    origin

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 app.use(function(req,res,next){
 	res.header('Access-Control-Origin','*');
-	res.header('Access-Control-Allow-Origin', 'http://sport-radar-front-end.herokuapp.com/');
+	res.header('Access-Control-Allow-Origin', 'http://sport-radar-front-end.herokuapp.com');
 	// res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
 	next();
